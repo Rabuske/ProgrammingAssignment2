f701c22ca428d07d247be71688f4587e05a8cb22
Rabuske <rabuske@outlook.com>
2015-01-21 00:42:43 -0200

    Final Version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These functions can be used together to create a "special matrix",
+# which store its inverse in cache, rather than compute it repeatedly 
 
-## Write a short comment describing this function
-
+# This function creates a "special matrix", which is in reality a list capable 
+# of store the inverse of the matrix passed as parameter. The functions get, 
+# set, getInverse and setInverse are available in the returned list. The 
+# functions get and set can be used to get and set the matrix itself and the 
+# functions getInverse and setInverse can be used to retrieve and store the 
+# inverse of the matrix in cache, respectively.
 makeCacheMatrix <- function(x = matrix()) {
+	# initialize the cache
+	inv <- NULL
 
+	# functions get and set of matrix itself (function set clears the cache)
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}	
+	get <- function() x
+	
+	# get and set of the inverse of the matrix in cache
+	setInverse <- function(inverse) inv <<- inverse	
+	getInverse <- function() inv
+	
+	# create and return a list with the above created functions
+	list(set = set, 
+		 get = get,
+		 setInverse = setInverse,
+		 getInverse = getInverse )	
 }
 
 
-## Write a short comment describing this function
-
+# This function returns the inverse of the "special matrix" created by
+# function makeCacheMatrix. If the cache is empty, the inverse is computed and
+# stored in the cache, otherwise the inverse from the cache is returned.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# get the inverse stored in cache
+    inv <- x$getInverse()
+	
+	# if the cache is not null, return the inverse from the cache
+	if(!is.null(inv)) return(inv)
+	
+	# get the matrix and compute its inverse
+	data <- x$get()
+	inv <- solve(data, ...)
+	
+	# store the inverse in the cache and returns it
+	x$setInverse(inv)
+	inv
 }
